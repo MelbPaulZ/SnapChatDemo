44a09a4d9d8580e577cfa1f5865d5085b3599378
MelbPaulZ <Paul@Pauls-MacBook-Pro.local>
2016-10-12 14:27:20 +1100

    todo: merge xiangxiang

---
+++ app/src/main/java/com/example/paul/snapchatdemo/fragment/FragmentCamera.java
@@ -0,0 +1,9 @@
+package com.example.paul.snapchatdemo.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Paul on 12/10/16.
+ */
+public class FragmentCamera extends Fragment {
+}
